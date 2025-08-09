def83d53cfd6f60bb0e3421b2892b8d4d49a13c7
cccesardiazzz <cccesardiazzz@icloud.com>
2025-08-08 22:16:08 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,19 @@
-const CACHE_NAME = 'miniboleta-cache-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+const CACHE_NAME = 'miniboleta-ui-v1';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-    )).then(() => self.clients.claim())
-  );
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k!==CACHE_NAME).map(k => caches.delete(k)))).then(()=>self.clients.claim()));
 });
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then(res => 
-      res || fetch(event.request).then(fetchRes => {
-        return caches.open(CACHE_NAME).then(cache => {
-          // Cache GET requests for offline
-          if (event.request.method === 'GET' && new URL(event.request.url).origin === location.origin) {
-            cache.put(event.request, fetchRes.clone());
-          }
-          return fetchRes;
-        });
-      }).catch(() => caches.match('./index.html'))
-    )
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(fr => {
+      if (e.request.method==='GET' && new URL(e.request.url).origin===location.origin){
+        const copy = fr.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, copy));
+      }
+      return fr;
+    }).catch(()=>caches.match('./index.html')))
   );
 });
